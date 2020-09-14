1383b791eb75871841850f4f1028d8e65adb07e3
Yegor <fox0899@gmail.com>
2020-09-14 04:49:13 +0400

    Updates

---
+++ precache-manifest.de7b70dae222330b77c9eb4ec2bd2191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3a9578836a11a1296a18f97285d439",
+    "url": "/forasoftChat/index.html"
+  },
+  {
+    "revision": "15e368dc214445557d7a",
+    "url": "/forasoftChat/static/css/main.6834936d.chunk.css"
+  },
+  {
+    "revision": "946119c389d540b8a746",
+    "url": "/forasoftChat/static/js/2.55b6e3ee.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/forasoftChat/static/js/2.55b6e3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e368dc214445557d7a",
+    "url": "/forasoftChat/static/js/main.e227f6c3.chunk.js"
+  },
+  {
+    "revision": "e1f44750ad99ad8f6f50",
+    "url": "/forasoftChat/static/js/runtime-main.fbbebdb5.js"
+  }
+]);
